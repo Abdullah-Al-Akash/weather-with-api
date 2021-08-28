9d58154422494b0549fa0f123084aaa11efad794
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-08-29 00:17:19 +0600

    Complete Weather

--- app.js
+++ app.js
@@ -4,24 +4,57 @@ const searchCity = () => {
         const searchField = document.getElementById('search-field');
         const searchFieldText = searchField.value;
         searchField.value = '';
+        // Without Search Something:
+        if (searchFieldText.length == 0) {
+                alert('Please Enter Your City Name');
+        }
         // console.log(searchFieldText);
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchFieldText}&appid=${apiKey}`)
-                .then(res => res.json())
-                .then(data => displayWeatherDetails(data))
+        else {
+                fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchFieldText}&appid=${apiKey}`)
+                        .then(res => res.json())
+                        .then(data => displayWeatherDetails(data))
+        }
 }
 
 const displayWeatherDetails = (data) => {
         console.log(data);
         const { main, weather } = data;
-        console.log(weather[0]);
         const city = document.getElementById('city');
         const temp = document.getElementById('temp');
         const situation = document.getElementById('situation');
         const pressure = document.getElementById('pressure');
+        const humidity = document.getElementById('humidity');
 
         // Set Every Update:
-        city.innerText = data.name;
-        temp.innerText = (main.temp - 273.15).toFixed(2);
-        situation.innerText = weather[0].main;
-        pressure.innerText = main.pressure;
+        if (data.name == undefined) {
+                city.innerText = 'There is no city';
+                temp.innerText = '';
+                situation.innerText = '';
+                pressure.innerText = '';
+                humidity.innerText = '';
+        }
+        else {
+                city.innerText = data.name;
+                temp.innerText = (main.temp - 273.15).toFixed(2);
+                situation.innerText = weather[0].main;
+                pressure.innerText = main.pressure;
+                humidity.innerText = main.humidity;
+
+                const weatherIcon = document.getElementById('weather-icon');
+                weatherIcon.textContent = ''
+                const div = document.createElement('div');
+                if (weather[0].main == "Clouds") {
+                        div.innerHTML = `
+                        <i class="bi bi-cloud-drizzle-fill icon"></i>
+                `;
+                        weatherIcon.appendChild(div);
+                }
+                else {
+                        div.innerHTML = `
+                        <i class="bi bi-brightness-high-fill icon"></i>
+                `;
+                        weatherIcon.appendChild(div);
+                }
+        }
+
 }
